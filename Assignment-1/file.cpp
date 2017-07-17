9f93e5f4678de11db1fe9fac20a521febf7a531a
alokedip20 <alokengineer14@gmail.com>
2017-07-18 02:17:44 +0530

    test file

---
+++ Assignment-1/file.cpp
@@ -0,0 +1,19 @@
+i = 0;
+for (int i = 0; i <= 10; i++)
+{
+	switch(i){
+		case 1:
+			break;
+	}
+	if(i >= 0){
+		i = i+2;
+	}
+	else{
+		i = i+1;
+	}
+	a = a&1;
+	if(a && 1 || a <= 45){
+		hello;
+	}
+	abcd_1243 = 1234;
+}
